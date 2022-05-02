4e97b2ead11acd55078fc393b0e1f167012366c4
IanGonzalezTDF <102770923+IanGonzalezTDF@users.noreply.github.com>
2022-05-02 18:06:11 -0300

    2-5 cambios

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,6 @@ let btnCalcular: number = document.getElementById("btnCalcular");
 
 let rotulo = document.getElementById("rotulo");
 
-btnEnvBase.addEventListener("click", () => {
-  console.log("la base es:", base.value);
-});
-
-btnEnvAltura.addEventListener("click", () => {
-  console.log("la base es:", altura.value);
-});
-
 btnCalcular.addEventListener("click", () => {
   console.log("el area es:", base.value * altura.value);
 });
