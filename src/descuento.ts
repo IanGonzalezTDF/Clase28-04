0205c220a4d7d3df655b8584178e499ae2f75835
IanGonzalezTDF <102770923+IanGonzalezTDF@users.noreply.github.com>
2022-05-02 22:19:12 -0300

    CalcularArea-Descuento

---
+++ src/descuento.ts
@@ -0,0 +1,5 @@
+let precioproducto: number = 450.5;
+let porcentajeDescuento: number = 0.1;
+let descuento: number = precioproducto * porcentajeDescuento;
+let preciofinal: number = precioproducto - descuento;
+console.log("El precio final es:", preciofinal);
