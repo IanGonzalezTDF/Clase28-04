737011db8637b1c80dfdd4663338d938ece903a4
IanGonzalezTDF <102770923+IanGonzalezTDF@users.noreply.github.com>
2022-05-24 11:41:03 -0300

    ??

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 btnCalcular.addEventListener("click", () => {
-  let btnEnvBase = document.getElementById("btnEnviarBase");
   let base = document.getElementById("base");
 
-  let btnEnvAltura = document.getElementById("btnEnviarAltura");
   let altura = document.getElementById("altura");
 
   let btnCalcular: number = document.getElementById("btnCalcular");
-
-  let rotulo = document.getElementById("rotulo");
 
   console.log("el area es:", base.value * altura.value);
 });
